3645f4b643053ad736e398bae1b936383f592109
Hitap98 <armnoori52@aho.com>
2021-03-29 17:04:14 +0430

    change insert

--- public/app.js
+++ public/app.js
@@ -4,7 +4,6 @@ const app = express();
 const router = app;
 const port = 3000;
 const bodyParser = require("body-parser");
-
 // Add the credentials to access your database
 var db = mysql.createConnection({
     host     : 'localhost',
@@ -15,17 +14,6 @@ var db = mysql.createConnection({
 
 app.use(bodyParser.json());
 app.use(express.static("./"));
-router.post('/get_doc_msgs', (req, res)=>{
-    var d_msg = "SELECT `CommonBaseTypeId`FROM `tblcommonbasetype`";
-    var d_msgs = [req.body.doc_id_msgs];
-        db.query(d_msg, d_msgs, (err,rows) => {
-            if(err){
-                console.log('error ', err);
-            } else { 
-                res.send(rows)
-            }
-        });
-    })
 router.post('/get_doc_msgs1', (req, res)=>{
 var d_msg = "SELECT * FROM `tblcommonbasetype`";
 var d_msgs = [req.body.doc_id_msgs];
@@ -38,6 +26,20 @@ var d_msgs = [req.body.doc_id_msgs];
     });
 })
 
+router.post('/addUser',(req, res)=>{
+    db.query('INSERT INTO `tblcommonbasetype` (CommonBaseTypeId, BaseTypeTitle) VALUES ("'+req.body.codeone+'","'+req.body.nameone+'")', (err,rows) => {
+        if(err){
+            console.log('error ', err);
+        } else { 
+console.log("insert1")        }
+    });
+    });
+    
+    // var insert = function (req)
+    // {
+    //     db.query('INSERT INTO tblcommonbasetyp (CommonBaseTypeId, BaseTypeTitle) VALUES ("'+req.body.codeone+'","'+req.body.nameone+'")');
+    // }
+        
 app.listen(port,()=>{
     console.log("connect")
 });
