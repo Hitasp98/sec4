70e83b2961aa29af8223c8db4efaceb0aa7e9d83
Hitap98 <armnoori52@aho.com>
2021-04-06 12:50:05 +0430

    public edit

--- public/app.js
+++ public/app.js
@@ -18,132 +18,30 @@ const partialsPath = path.join(__dirname, './html')
 app.set('view engine', 'hbs')
 app.set('views', viewsPath)
 hbs.registerPartials(partialsPath)
-
+app.use(express.json());
 const db = require('../my-database/config')
 app.use(bodyParser.json());
 app.use(express.static("./html/"));
+app.use(express.urlencoded({ extended: true }));
+const productRouter=require('./router/ProductRouter')
+app.use('/product',productRouter)
 
-//select tblcommonbasetype
-router.post("/selecttblcommonbasetype", (req, res) => {
-  var d_msg = "SELECT * FROM `tblcommonbasetype`";
-  var d_msgs = [req.body.doc_id_msgs];
-  db.query(d_msg, d_msgs, (err, rows) => {
-    if (err) {
-      console.log("error ", err);
-    } else {
-      res.send(rows);
-    }
-  });
-});
-// select db 
-router.post("/selecttblcommonbasedata", (req, res) => {
-  var d_msg = "SELECT * FROM `tblcommonbasedata`";
-  var d_msgs = [req.body.doc_id_msgs];
-  db.query(d_msg, d_msgs, (err, rows) => {
-    if (err) {
-      console.log("error ", err);
-    } else {
-      res.send(rows);
-    }
-  });
-});
-router.post("/addUser", (req, res) => {
-  db.query(
-    'INSERT INTO `tblcommonbasetype` (CommonBaseTypeId, BaseTypeTitle) VALUES ("' +
-    req.body.codeone +
-    '","' +
-    req.body.nameone +
-    '")',
-    (err, rows) => {
-      if (err) {
-        console.log("error ", err);
-      } else {
-        console.log("insert");
-      }
-    }
-  );
-});
-router.post("/EditUser", (req, res) => {
-  // "UPDATE users SET name='"+req.body.nameone+"' where id='"+req.body.codeone+"'"
-  db.query("UPDATE `tblcommonbasetype` SET `BaseTypeTitle`='" + req.body.nameone + "' where `CommonBaseTypeId`=" + req.body.codeone + ""),
-    (err, rows) => {
-      if (err) {
-        console.log("error ", err);
-      } else {
-        console.log("Update");
-      }
-    }
-});
-
-router.post("/DeleteUser", (req, res) => {
-  // DELETE FROM `tblcommonbasetype` WHERE `tblcommonbasetype`.`CommonBaseTypeId` = 14"?
-  db.query("DELETE FROM `tblcommonbasetype` WHERE `CommonBaseTypeId`=" + req.body.codeone + ""),
-    (err, rows) => {
-      if (err) {
-        console.log("error ", err);
-      } else {
-        console.log("Delete");
-      }
-    }
-});
+require('./initDB')();
 
 
 
 
-//select db 	tblcommonbasedata
-router.post("/selecttblcommonbasetype", (req, res) => {
-  var d_msg = "SELECT * FROM `tblcommonbasetype`";
-  var d_msgs = [req.body.doc_id_msgs];
-  db.query(d_msg, d_msgs, (err, rows) => {
-    if (err) {
-      console.log("error ", err);
-    } else {
-      res.send(rows);
-    }
-  });
-});
-router.post("/addUserTwo", (req, res) => {
-  db.query(
-    'INSERT INTO `tblcommonbasedata` (BaseCode   , BaseValue, CommonBaseDataId ) VALUES ("' +
-    req.body.codeone +
-    '","' +
-    req.body.nameone +
-    '","' + req.body.idrandom + '")',
-    (err, rows) => {
-      if (err) {
-        console.log("error ", err);
-      } else {
-        console.log("insert");
-      }
-    }
-  );
-});
-router.post("/EditUserTwo", (req, res) => {
-  // "UPDATE users SET name='"+req.body.nameone+"' where id='"+req.body.codeone+"'"
-  db.query("UPDATE `tblcommonbasedata` SET `BaseValue`='" + req.body.nameone + "' where `BaseCode`=" + req.body.codeone + ""),
-    (err, rows) => {
-      if (err) {
-        console.log("error ", err);
-      } else {
-        console.log("Update");
-      }
-    }
-});
 
-router.post("/DeleteUserTwo", (req, res) => {
-  // DELETE FROM `tblcommonbasetype` WHERE `tblcommonbasetype`.`CommonBaseTypeId` = 14"?
-  db.query("DELETE FROM `tblcommonbasedata` WHERE `BaseCode`=" + req.body.codeone + ""),
-    (err, rows) => {
-      if (err) {
-        console.log("error ", err);
-      } else {
-        console.log("Delete");
-      }
-    }
-});
+
 app.get('/*', (req, res) => {
   res.render('404')})
 
 app.listen(port, () => {
   console.log("connect");
 });
+
+
+
+
+
+
